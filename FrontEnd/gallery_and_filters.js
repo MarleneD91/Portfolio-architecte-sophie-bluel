3dfc744dc1512f2bf87a5f228befd2aecd0ae9f3
Marlène D <marlene.a.diaz@gmail.com>
2023-03-07 12:19:33 +0100

    working on login.js but error 405 w/ POST method

--- FrontEnd/gallery_and_filters.js
+++ FrontEnd/gallery_and_filters.js
@@ -15,51 +15,6 @@ allCategories.setAttribute("id", "all");
 allCategories.innerText = "Tous";
 filtersDiv.appendChild(allCategories);
 
-
-
-
-
-
-/* -------- 1 a. - RETRIEVING WORKS W/ FETCH + INSERTING INTO DOM -------- */
-
-
-
-
-async function fetchData () {
-    const fetchProjects = await fetch ("http://localhost:5678/api/works");
-    var projects = await fetchProjects.json();
-    console.log(projects);
-    if (fetchProjects.ok) {
-        return projects;
-    } else {
-        console.log ("error");
-    };
-
-
-};
-fetchData()
-.then( projects => {
-    gallery.innerHTML = " ";
-    // function for in order to loop in the json array
-    for (let i=0; i<projects.length; i++) { // For each project in the data array
-        const projectInfo = projects[i]; // using the i element (object) of the array
-        /* figure element */
-        const projectFigure = document.createElement("figure"); // the figure tag
-        const figureClass = "project-" + projectInfo.id; // class name = project-idnumber for figure tag
-        const figureId = projectFigure.setAttribute("id", figureClass); //adding id
-        projectFigure.setAttribute("class", projectInfo.category.name)//Add class=category name for the filters
-        gallery.appendChild(projectFigure); // adding figure tag to the gallery div
-        /* img element */
-        const projectImg = document.createElement("img"); // creating the img tag
-        projectImg.src = projectInfo.imageUrl; // adding the url to the src attribute for each img tag
-        projectFigure.appendChild(projectImg); // append img tag inside the figure tag
-        /* figcaption element */
-        const projectCaption = document.createElement("figcaption"); // creating the fig caption
-        projectCaption.innerText = projectInfo.title; // adding text = title of the i object, into the figcaption tag
-        projectFigure.appendChild(projectCaption); // append figcaption inside figure tag
-    }
-});
-
 /* Filter function */
 async function forCategories() {
     //Retrieve categories
@@ -83,14 +38,59 @@ async function forCategories() {
             //categoryButton.addEventListener("click", filterOnClick());
         };
     });
-/*.then(projects => filterOnClick(projects));
-function filterOnClick()
-    {
-    const projectsFiltered = projects.filter( function (){
-            return categoryId == figureID ;
-            });   
-        };          
 
+
+
+
+
+
+/* -------- 1 a. - RETRIEVING WORKS W/ FETCH + INSERTING INTO DOM -------- */
+
+
+
+
+async function fetchData () {
+    const fetchProjects = await fetch ("http://localhost:5678/api/works");
+    const projects = await fetchProjects.json();
+    console.log(projects);
+    if (fetchProjects.ok) {
+        return projects;
+    } else {
+        console.log ("error");
+    };
+};
+fetchData()
+.then( projects => {
+    gallery.innerHTML = " ";
+    // function for in order to loop in the json array
+    for (let i=0; i<projects.length; i++) { // For each project in the data array
+        const projectInfo = projects[i]; // using the i element (object) of the array
+        /* figure element */
+        const projectFigure = document.createElement("figure"); // the figure tag
+        const figureClass = "project-" + projectInfo.id; // class name = project-idnumber for figure tag
+        const figureId = projectFigure.setAttribute("id", figureClass); //adding id
+        projectFigure.setAttribute("class", projectInfo.category.name)//Add class=category name for the filters
+        gallery.appendChild(projectFigure); // adding figure tag to the gallery div
+        /* img element */
+        const projectImg = document.createElement("img"); // creating the img tag
+        projectImg.src = projectInfo.imageUrl; // adding the url to the src attribute for each img tag
+        projectFigure.appendChild(projectImg); // append img tag inside the figure tag
+        /* figcaption element */
+        const projectCaption = document.createElement("figcaption"); // creating the fig caption
+        projectCaption.innerText = projectInfo.title; // adding text = title of the i object, into the figcaption tag
+        projectFigure.appendChild(projectCaption); // append figcaption inside figure tag
+    }
+    
+});
+
+
+
+
+
+
+/*function filterOnClick (){
+    
+}
 
 
 
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,101 +1,45 @@
 /* -------- LOGIN SECTION -------- */
 
 
-const loginClick = function clickOnLogin(event) {
-    event.preventDefault();
-// 1 -HTML  (+CSS) STRUCTURE
-    var main = document.querySelector("main");
-    main.innerHTML = "";
 
-    // login section 
-    const loginSection = document.createElement("section");
-    loginSection.classList.add("login");
-    main.appendChild(loginSection);
-
-    // title
-    const loginTitle = document.createElement("h2");
-    loginTitle.innerText = "Log In";
-    loginTitle.classList.add("loginTitle");
-    loginSection.appendChild(loginTitle);
-
-    // Form
-    const form = document.createElement("form");
-    form.setAttribute("id","login-form");
-    form.setAttribute("method","post");
-    loginSection.appendChild(form);
-        //email
-        const mailLabel = document.createElement("label");
-        mailLabel.setAttribute("for","email");
-        mailLabel.innerHTML = "E-mail";
-        form.appendChild(mailLabel);
-        const mailInput = document.createElement("input");
-        mailInput.setAttribute("type","email");
-        mailInput.setAttribute("id","email");
-        mailInput.setAttribute("required","");
-        form.appendChild(mailInput);
-        //password
-        const passwordLabel = document.createElement("label");
-        passwordLabel.setAttribute("for","password");
-        passwordLabel.innerHTML = "Mot de passe";
-        form.appendChild(passwordLabel);
-        const passwordInput = document.createElement("input");
-        passwordInput.setAttribute("type","password");
-        passwordInput.setAttribute("id","password");
-        passwordInput.setAttribute("required","");
-        form.appendChild(passwordInput);
-        //submit
-        var submit = document.createElement("input");
-        submit.setAttribute("type","submit");
-        submit.setAttribute("id","submit-button");
-        submit.setAttribute("value","Se connecter");
-        submit.addEventListener("click", submitLogin);
-        form.appendChild(submit);
-        //password forgotten
-        const forgotPassword =  document.createElement("a");
-        forgotPassword.setAttribute("href","#forgot-password");
-        forgotPassword.setAttribute("class","forgot-password");
-        forgotPassword.innerText = "Mot de passe oublié";
-        form.appendChild(forgotPassword);
-    };
-
-// 2- EVENT LISTENER, click on nav 'login'
-const navLogin = document.querySelector("#login");
-navLogin.addEventListener("click", loginClick);
-
-// 3- POST THE ID/PSWD, ALERT IF SUCCESS OR NOT
+/* -------------------- POST THE ID/PSWD, ALERT IF SUCCESS OR NOT -------------------- */
 
 // submit id + pwd function
-async function submitLogin(event) {
-        event.preventDefault();
-        // body elemnts
+async function submitLogin(e) {
+        
+        // body elemnets
         const email = document.querySelector("#email").value;
         const password = document.querySelector("#password").value;
         // define fetch post config object
         const postMethod = {
             method: "POST",
-            headers: { "Content-Type": "application/json" },
+            headers: { "Content-Type": "application/json",
+                        "Accept" : "application/json" },
             body: JSON.stringify({
                 email: email,
                 password: password,
-            })
-        }
+            }),
+        };
         // Fetch post
-        const postInputs = await fetch("http://localhost:5678/api/users/login", postMethod);
+        const postInputs = await fetch('http://localhost:5678/api/users/login', postMethod);
         const fetchPostData = await postInputs.json();
         console.log(fetchPostData);
         console.log(postInputs);
         if (postInputs.status == 200){
             alert("Bienvenue sur le site!");
             window.localStorage.setItem("token", fetchPostData.token);
-            window.location.replace("");
+            window.location.replace("./index.html");
         } else if (postInputs.status == 401) {
                 alert ("Mot de passe invalide");
         } else if (postInputs.status == 404) {
                 alert("Mot de passe et/ou identifiant invalide(s)")
         }   
+        e.preventDefault();
     };        
      
-    
+// add the submit event listener on the login button
+var loginButton = document.querySelector("#submit-button");
+loginButton.addEventListener("submit", submitLogin);
    
 
 
