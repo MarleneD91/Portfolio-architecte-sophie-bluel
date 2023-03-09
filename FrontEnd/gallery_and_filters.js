a4868c10285b03dc39dca46a73aa61de013dde91
Marlène D <marlene.a.diaz@gmail.com>
2023-03-09 13:51:18 +0100

    Filters done, work - comments added, code cleaned (spaces, useless lines, etc.)

--- FrontEnd/gallery_and_filters.js
+++ FrontEnd/gallery_and_filters.js
@@ -1,54 +1,27 @@
+/* Selecting the gallery + Initializing arrays where the datas will be stored */
 const gallery = document.querySelector('.gallery');
-/* ------------------------------------------------------------------------- */
+let categoriesArray = new Set();
+let allProjects = [];
 
+/* ------ CREATING ELEMENTS THAT DON'T NEED DATA ------ */
 
-/* ------ Categories filter ------ */
-
-/* creating the filters div */
+/* Creating the filters div */
 const filtersDiv = document.createElement("div");
 filtersDiv.classList.add("filters"); // add the "filter" class name
 document.querySelector('#portfolio').insertBefore(filtersDiv,gallery); // Inserting the filter section before the gallery²
+
 /* Adding the buttton 'Tous' - never change*/
-const allCategories = document.createElement("button");
-allCategories.classList.add("filter");
-allCategories.setAttribute("id", "all");
-allCategories.innerText = "Tous";
-filtersDiv.appendChild(allCategories);
+const allCategoriesButton = document.createElement("button");
+allCategoriesButton.classList.add("filter");
+allCategoriesButton.setAttribute("id", "all");
+allCategoriesButton.setAttribute("class","filter");
+allCategoriesButton.innerText = "Tous";
+filtersDiv.appendChild(allCategoriesButton);
 
-/* Filter function */
-async function forCategories() {
-    //Retrieve categories
-    const fetchCategories = await fetch ("http://localhost:5678/api/categories");
-    const categories = await fetchCategories.json();
-    console.log(categories);
-    if (fetchCategories.ok) {
-        return categories;
-    } else {
-        console.log ("error");
-    };
-    };
-    forCategories().then(categories =>{
-        for(let i = 0; i <categories.length; i++){ 
-            const categoryButton = document.createElement("button");
-            categoryButton.classList.add("filter");
-            const categoryId = categories[i].name;
-            categoryButton.setAttribute("id", categoryId)
-            categoryButton.innerText = categories[i].name;
-            filtersDiv.appendChild(categoryButton);
-            //categoryButton.addEventListener("click", filterOnClick());
-        };
-    });
+/* -------- RETRIEVING WORKS W/ FETCH : INSERTED INTO THE GALLERY --------
+-------- / SELECTED BY CATEGORY-LINKED TO BUTTON CATEGORY -------- */
 
-
-
-
-
-
-/* -------- 1 a. - RETRIEVING WORKS W/ FETCH + INSERTING INTO DOM -------- */
-
-
-
-
+// fetch the data / HTTP get request
 async function fetchData () {
     const fetchProjects = await fetch ("http://localhost:5678/api/works");
     const projects = await fetchProjects.json();
@@ -59,45 +32,72 @@ async function fetchData () {
         console.log ("error");
     };
 };
+
 fetchData()
-.then( projects => {
+.then( projects => { // use the .json response to store the collected datas
+
     gallery.innerHTML = " ";
-    // function for in order to loop in the json array
-    for (let i=0; i<projects.length; i++) { // For each project in the data array
-        const projectInfo = projects[i]; // using the i element (object) of the array
-        /* figure element */
-        const projectFigure = document.createElement("figure"); // the figure tag
-        const figureClass = "project-" + projectInfo.id; // class name = project-idnumber for figure tag
-        const figureId = projectFigure.setAttribute("id", figureClass); //adding id
-        projectFigure.setAttribute("class", projectInfo.category.name)//Add class=category name for the filters
-        gallery.appendChild(projectFigure); // adding figure tag to the gallery div
-        /* img element */
-        const projectImg = document.createElement("img"); // creating the img tag
-        projectImg.src = projectInfo.imageUrl; // adding the url to the src attribute for each img tag
-        projectFigure.appendChild(projectImg); // append img tag inside the figure tag
-        /* figcaption element */
-        const projectCaption = document.createElement("figcaption"); // creating the fig caption
-        projectCaption.innerText = projectInfo.title; // adding text = title of the i object, into the figcaption tag
-        projectFigure.appendChild(projectCaption); // append figcaption inside figure tag
-    }
+
+    /* Loop to get the datas (projects infos + categories) into arrays */
+    for (let i=0; i<projects.length; i++) {
+        const projectInfo = projects[i]; 
+        categoriesArray.add(projectInfo.category.name);       
+        allProjects.push(projectInfo);
+    };
+    //console.log(allProjects); -- check if the datas are retrieved
+    //console.log(categoriesArray); -- same
+
+    /* Creating the filters buttons */
+    categoriesArray.forEach(function createButton(category){
+        const categoryButton = document.createElement("button");
+        categoryButton.classList.add("filter");
+        const categoryName = category;
+        categoryButton.setAttribute("id", categoryName)
+        categoryButton.innerText = categoryName;
+        categoryButton.setAttribute("class","filter");
+        filtersDiv.appendChild(categoryButton);
+    });
+    
+    /* function to create the projects cards */
+    function displayProjects(array){
+        for (let i=0; i<array.length; i++){
+            const projectData = array[i]; // using the i element (object) of the array
+            /* figure element */
+            const projectFigure = document.createElement("figure"); // the figure tag
+            const projectClassTxt = "project" + " " + projectData.category.name;
+            gallery.appendChild(projectFigure); // adding figure tag to the gallery div
+            /* img element */
+            const projectImg = document.createElement("img"); // creating the img tag
+            projectImg.src = projectData.imageUrl; // adding the url to the src attribute for each img tag
+            projectFigure.appendChild(projectImg); // append img tag inside the figure tag
+            /* figcaption element */
+            const projectCaption = document.createElement("figcaption"); // creating the fig caption
+            projectCaption.innerText = projectData.title; // adding text = title of the i object, into the figcaption tag
+            projectFigure.appendChild(projectCaption); // append figcaption inside figure tag*/
+        };
+    };
+
+    displayProjects(projects); // To display all the projects when the page load
+
+    let buttonId = null;  
+    /* Onclick fonction that allows to display projects corresponding to the selected category */
+    const filterOnClick = function(e) {
+        e.preventDefault();
+        buttonId = e.target.getAttribute("id");
+        console.log(buttonId);
+        if (buttonId == "all"){ // For the "Tous" button, display all the projects
+            displayProjects(projects);
+        } else { // For the others button, the projects displayed match the buttons categories
+            gallery.innerHTML = "";
+                const filteredProjects = projects.filter(function(project){
+                    return project.category.name == buttonId;
+                });
+                console.log(filteredProjects);
+                displayProjects(filteredProjects);
+        };
+    };
+    /* Add the event listener on the categories buttons */
+    document.querySelectorAll(".filter").forEach(button => button.addEventListener("click",filterOnClick));    
     
 });
-
-
-
-
-
-
-/*function filterOnClick (){
-    
-}
-
-
-
-
-//function filterByCategory ()
-
-
-
-
-/* add click event on buttons*/
+/* --------------------------------- END OF THE GALLERY AND FILTERS SCRIPT --------------------------------- */
